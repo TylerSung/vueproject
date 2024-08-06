e048de3842e66d54f6a19f31fc1a0f351178d916
cyberobject <117184986+cyberobjec@users.noreply.github.com>
2024-08-06 22:09:21 +0800

    banner分类完成2

--- rabbit/src/views/Home/components/HomeBanner.vue
+++ rabbit/src/views/Home/components/HomeBanner.vue
@@ -1,13 +1,43 @@
-<template>
-  <div>
+<script setup>
+import getBannerList from "@/apis/banner";
 
+import { ref, onMounted } from "vue";
+
+const bannerListRef = ref([]);
+
+const getBanner = async () => {
+  const res = await getBannerList();
+
+  bannerListRef.value = res.result;
+};
+
+onMounted(() => {
+  getBanner();
+});
+</script>
+
+<template>
+  <div class="home-banner">
+    <el-carousel height="500px">
+      <el-carousel-item v-for="item in bannerListRef" :key="item.id">
+        <img :src="item.imgUrl" alt="" />
+      </el-carousel-item>
+    </el-carousel>
   </div>
 </template>
 
-<script setup lang="ts">
+<style scoped lang="scss">
+.home-banner {
+  width: 1240px;
+  height: 500px;
+  position: absolute;
+  left: 0;
+  top: 0;
+  z-index: 98;
 
-</script>
-
-<style scoped>
-
+  img {
+    width: 100%;
+    height: 500px;
+  }
+}
 </style>
